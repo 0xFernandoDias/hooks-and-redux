0d0beee63c2cbfd7417c9f393fbed57ec588ed32
nandOdiaz <fernando_dias99@hotmail.com>
2021-07-01 14:38:14 -0300

    use state hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [repositories, setRepositories] = useState([
+    {id: 1, name: 'repo-1'},
+    {id: 2, name: 'repo-2'},
+    {id: 3, name: 'repo-3'}
+  ])
+
+  function handleAddRepository() {
+    setRepositories([
+      ...repositories,
+      {id: repositories.length + 1, name: `repo-${repositories.length + 1}`}
+    ])
+
+    console.log(repositories[repositories.length - 1])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+    <ul className="list-group">
+      {repositories.map(element => {
+        return (
+        <li className="list-group-item" key={element.id}>
+          {element.name}
+        </li>
+        )
+      })}
+    </ul>
+    <button className='btn btn-primary' onClick={handleAddRepository}>
+      Add repository
+    </button>
+   </>
+  )
 }
 
-export default App;
+export default App
